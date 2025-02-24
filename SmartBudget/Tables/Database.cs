a7e649020fd6f6d057f491d7b5502ab4c1c25b2d
Антон Смирнов <smirnovanton443@gmail.com>
2025-02-24 15:58:20 +0300

    Дополнение базы данных

--- SmartBudget/Tables/Database.cs
+++ SmartBudget/Tables/Database.cs
@@ -24,7 +24,22 @@ namespace SmartBudget.Tables
                         Password TEXT NOT NULL
                     );";
 
+                string createCostTable = @"
+                    CREATE TABLE IF NOT EXISTS Costs (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        UserId INTEGER NOT NULL,
+                        Amount REAL NOT NULL,
+                        Category TEXT NOT NULL,
+                        PurchaseDate DATETIME NOT NULL,
+                        FOREIGN KEY (UserId) REFERENCES Users(Id)
+                    )";
+
                 using (var command = new SqliteCommand(createUserTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqliteCommand(createCostTable, connection))
                 {
                     command.ExecuteNonQuery();
                 }
